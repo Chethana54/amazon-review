a51174372d3037f7d795bc6ec28b2155876c0cc9
Chethana54 <84118724+Chethana54@users.noreply.github.com>
2021-05-26 20:46:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model = joblib.load('amazon review')
+st.title('AMAZON REVIEW PREDICTION')
+ip = st.text_input("enter the message")
+op = model.predict([ip])
+if st.button('predict'):
+  st.title(op[0])
