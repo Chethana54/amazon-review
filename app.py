b005dbf2bd4c5e1375676268737b3e02b3f78df1
Chethana54 <84118724+Chethana54@users.noreply.github.com>
2021-05-26 20:10:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-!pip install streamlit --quiet
-!pip install pyngrok==4.1.1
-from pyngrok import ngrok
-%%writefile app.py
+
 import streamlit as st
 import joblib
 model = joblib.load('amazon review')
@@ -10,7 +7,5 @@ ip = st.text_input("enter the message")
 op = model.predict([ip])
 if st.button('predict'):
   st.title(op[0])
-!nohup streamlit run app.py &
-url = ngrok.connect(port='8501')
-url
+
 
